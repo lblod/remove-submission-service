785e1e6aaa3596066731456801ae29d7f269bbe6
benjay10 <ben.behaeghe@redpencil.io>
2023-07-25 03:37:00 +0200

    Find submission graph and use in all queries
    All data about tasks, jobs and the submission is stored in the graph of
    the organisation. Use the organisation UUID to recreate the graph and
    use that graph for all reads and deletes.

--- delete.js
+++ delete.js
@@ -1,7 +1,25 @@
 import { sparqlEscapeString, sparqlEscapeUri } from 'mu';
 import { querySudo } from '@lblod/mu-auth-sudo';
-import { deleteFile, FILE_GRAPH } from './file-helpers';
+import { deleteFile } from './file-helpers';
 import { SparqlJsonParser } from 'sparqljson-parse';
+import * as env from 'env-var';
+
+const GRAPH_TEMPLATE = env
+  .get('GRAPH_TEMPLATE')
+  .example(
+    'http://mu.semte.ch/graphs/organizations/~ORGANIZATION_ID~/LoketLB-toezichtGebruiker',
+  )
+  .default(
+    'http://mu.semte.ch/graphs/organizations/~ORGANIZATION_ID~/LoketLB-toezichtGebruiker',
+  )
+  .asUrlString();
+
+(function checkEnvVars() {
+  if (!/~ORGANIZATION_ID~/g.test(GRAPH_TEMPLATE))
+    throw new Error(
+      `The GRAPH_TEMPLATE environment variable ${GRAPH_TEMPLATE} does not contain a ~ORGANIZATION_ID~.`,
+    );
+})();
 
 const SENT_STATUS =
   'http://lblod.data.gift/concepts/9bd8d86d-bb10-4456-a84e-91e9507c374c';
@@ -26,7 +44,12 @@ const META_FILE_TYPE = 'http://data.lblod.gift/concepts/meta-file-type';
  * `message` (string) properties.
  */
 export async function deleteSubmission(uuid) {
-  const submissionInfo = await getSubmissionById(uuid);
+  const organisationId = await getOrganisationIdFromSubmission(uuid);
+  const submissionGraph = GRAPH_TEMPLATE.replace(
+    '~ORGANIZATION_ID~',
+    organisationId,
+  );
+  const submissionInfo = await getSubmissionById(uuid, submissionGraph);
 
   if (submissionInfo) {
     const {
@@ -38,19 +61,21 @@ export async function deleteSubmission(uuid) {
     } = submissionInfo;
     if (status !== SENT_STATUS) {
       // if not a auto-submission, nothing was harvested
-      if (taskURI) await deleteHarvestedFiles(submissionURI);
+      if (taskURI) await deleteHarvestedFiles(submissionURI, submissionGraph);
 
-      if (formDataURI) await deleteUploadedFiles(formDataURI);
+      if (formDataURI) await deleteUploadedFiles(formDataURI, submissionGraph);
       if (submissionDocumentURI)
-        await deleteLinkedTTLFiles(submissionDocumentURI);
+        await deleteLinkedTTLFiles(submissionDocumentURI, submissionGraph);
 
       // if not a auto-submission, no task was created
-      if (taskURI) await deleteResource(taskURI);
+      if (taskURI)
+        await deleteResource(taskURI, submissionGraph, submissionGraph);
 
-      if (formDataURI) await deleteResource(formDataURI);
-      if (submissionDocumentURI) await deleteResource(submissionDocumentURI);
+      if (formDataURI) await deleteResource(formDataURI, submissionGraph);
+      if (submissionDocumentURI)
+        await deleteResource(submissionDocumentURI, submissionGraph);
 
-      await deleteResource(submissionURI);
+      await deleteResource(submissionURI, submissionGraph);
       return { message: `successfully deleted submission <${submissionURI}>.` };
     }
     return {
@@ -73,13 +98,13 @@ export async function deleteSubmission(uuid) {
  * Private
  */
 
-async function deleteHarvestedFiles(uri) {
-  const files = await getHarvestedFiles(uri);
+async function deleteHarvestedFiles(uri, graph) {
+  const files = await getHarvestedFiles(uri, graph);
   for (const file of files) {
-    await deleteFile(file.parent);
-    await deleteFile(file.location);
-    await deleteResource(file.location);
-    await deleteResource(file.file);
+    await deleteFile(file.parent, graph);
+    await deleteFile(file.location, graph);
+    await deleteResource(file.location, graph);
+    await deleteResource(file.file, graph);
   }
 }
 
@@ -88,12 +113,12 @@ async function deleteHarvestedFiles(uri) {
  *
  * @param uri of the resource to delete the linked files for
  */
-async function deleteUploadedFiles(uri) {
-  const files = await getUploadedFiles(uri);
+async function deleteUploadedFiles(uri, graph) {
+  const files = await getUploadedFiles(uri, graph);
   for (const file of files) {
-    await deleteFile(file.location);
-    await deleteResource(file.file);
-    await deleteResource(file.location);
+    await deleteFile(file.location, graph);
+    await deleteResource(file.file, graph);
+    await deleteResource(file.location, graph);
   }
 }
 
@@ -102,16 +127,16 @@ async function deleteUploadedFiles(uri) {
  *
  * @param uri resource (submission-document) to delete the linked ttl files for
  */
-async function deleteLinkedTTLFiles(uri) {
-  const additionsFile = await getTTLResource(uri, ADDITIONS_FILE_TYPE);
-  const removalsFile = await getTTLResource(uri, REMOVALS_FILE_TYPE);
-  const metaFile = await getTTLResource(uri, META_FILE_TYPE);
-  const sourceFile = await getTTLResource(uri, FORM_DATA_FILE_TYPE);
+async function deleteLinkedTTLFiles(uri, graph) {
+  const additionsFile = await getTTLResource(uri, ADDITIONS_FILE_TYPE, graph);
+  const removalsFile = await getTTLResource(uri, REMOVALS_FILE_TYPE, graph);
+  const metaFile = await getTTLResource(uri, META_FILE_TYPE, graph);
+  const sourceFile = await getTTLResource(uri, FORM_DATA_FILE_TYPE, graph);
 
-  if (additionsFile) await deleteFile(additionsFile);
-  if (removalsFile) await deleteFile(removalsFile);
-  if (metaFile) await deleteFile(metaFile);
-  if (sourceFile) await deleteFile(sourceFile);
+  if (additionsFile) await deleteFile(additionsFile, graph);
+  if (removalsFile) await deleteFile(removalsFile, graph);
+  if (metaFile) await deleteFile(metaFile, graph);
+  if (sourceFile) await deleteFile(sourceFile, graph);
 }
 
 /**
@@ -119,16 +144,18 @@ async function deleteLinkedTTLFiles(uri) {
  *
  * @param uri of the resource.
  */
-async function getUploadedFiles(uri) {
+async function getUploadedFiles(uri, graph) {
   const response = await querySudo(`
     PREFIX dct: <http://purl.org/dc/terms/>
     PREFIX nie: <http://www.semanticdesktop.org/ontologies/2007/01/19/nie#>
 
     SELECT DISTINCT ?file ?location ?parent
     WHERE {
+      GRAPH ${sparqlEscapeUri(graph)} {
         ${sparqlEscapeUri(uri)} dct:hasPart ?file .
         ?location nie:dataSource ?file .
         OPTIONAL {?parent nie:dataSource ?location .}
+      }
     }
   `);
 
@@ -149,16 +176,18 @@ async function getUploadedFiles(uri) {
  *
  * @param uri of the resource.
  */
-async function getHarvestedFiles(uri) {
+async function getHarvestedFiles(uri, graph) {
   const response = await querySudo(`
     PREFIX dct: <http://purl.org/dc/terms/>
     PREFIX nie: <http://www.semanticdesktop.org/ontologies/2007/01/19/nie#>
 
     SELECT DISTINCT ?file ?location ?parent
     WHERE {
-      ${sparqlEscapeUri(uri)} nie:hasPart ?file .
-      ?location nie:dataSource ?file .
-      ?parent nie:dataSource ?location .
+      GRAPH ${sparqlEscapeUri(graph)} {
+        ${sparqlEscapeUri(uri)} nie:hasPart ?file .
+        ?location nie:dataSource ?file .
+        ?parent nie:dataSource ?location .
+      }
     }
   `);
 
@@ -181,7 +210,7 @@ async function getHarvestedFiles(uri) {
  * @param {string} submissionDocument URI of the submitted document to get the related file for
  * @param {string} fileType URI of the type of the related file
  */
-async function getTTLResource(submissionDocument, fileType) {
+async function getTTLResource(submissionDocument, fileType, graph) {
   const response = await querySudo(`
     PREFIX mu: <http://mu.semte.ch/vocabularies/core/>
     PREFIX dct: <http://purl.org/dc/terms/>
@@ -189,10 +218,8 @@ async function getTTLResource(submissionDocument, fileType) {
 
     SELECT DISTINCT ?file
     WHERE {
-      GRAPH ?g {
+      GRAPH ${sparqlEscapeUri(graph)} {
         ${sparqlEscapeUri(submissionDocument)} dct:source ?file .
-      }
-      GRAPH ${sparqlEscapeUri(FILE_GRAPH)} {
         ?file dct:type ${sparqlEscapeUri(fileType)} .
       }
     } LIMIT 1
@@ -212,22 +239,22 @@ async function getTTLResource(submissionDocument, fileType) {
  *
  * @param {string} URI of the resource to delete the related files for
  */
-async function deleteResource(URI) {
+async function deleteResource(uri, graph) {
   return querySudo(`
     DELETE {
-      GRAPH ?g {
-        ${sparqlEscapeUri(URI)} ?p ?o .
+      GRAPH ${sparqlEscapeUri(graph)} {
+        ${sparqlEscapeUri(uri)} ?p ?o .
       }
     }
     WHERE {
-      GRAPH ?g {
-        ${sparqlEscapeUri(URI)} ?p ?o .
+      GRAPH ${sparqlEscapeUri(graph)} {
+        ${sparqlEscapeUri(uri)} ?p ?o .
       }
     }
   `);
 }
 
-async function getSubmissionById(submissionId) {
+async function getSubmissionById(submissionId, graph) {
   const infoQuery = `
     PREFIX meb: <http://rdf.myexperiment.org/ontologies/base/>
     PREFIX mu: <http://mu.semte.ch/vocabularies/core/>
@@ -239,27 +266,30 @@ async function getSubmissionById(submissionId) {
     PREFIX task: <http://redpencil.data.gift/vocabularies/tasks/>
     PREFIX taskO: <http://lblod.data.gift/id/jobs/concept/TaskOperation/>
 
-    SELECT DISTINCT ?submission ?formData ?submissionTask ?submissionDocument ?status WHERE {
-      ?submission a meb:Submission;
-         mu:uuid ${sparqlEscapeString(submissionId)};
-         adms:status ?status.
+    SELECT DISTINCT ?submission ?formData ?submissionTask ?submissionDocument ?status
+    WHERE {
+      GRAPH ${sparqlEscapeUri(graph)} {
+        ?submission a meb:Submission;
+           mu:uuid ${sparqlEscapeString(submissionId)};
+           adms:status ?status.
 
-      OPTIONAL {
-        ?submission prov:generated ?formData.
-        ?formData a melding:FormData.
-      }
+        OPTIONAL {
+          ?submission prov:generated ?formData.
+          ?formData a melding:FormData.
+        }
 
-      OPTIONAL {
-        ?submissionTask
-          a task:Task ;
-          task:operation taskO:register ;
-          dct:isPartOf ?job .
-        ?job prov:generated ?submission.
-      }
+        OPTIONAL {
+          ?submissionTask
+            a task:Task ;
+            task:operation taskO:register ;
+            dct:isPartOf ?job .
+          ?job prov:generated ?submission.
+        }
 
-      OPTIONAL {
-        ?submission dct:subject ?submissionDocument.
-        ?submissionDocument a ext:SubmissionDocument.
+        OPTIONAL {
+          ?submission dct:subject ?submissionDocument.
+          ?submissionDocument a ext:SubmissionDocument.
+        }
       }
     } LIMIT 1
   `;
@@ -277,4 +307,21 @@ async function getSubmissionById(submissionId) {
       taskURI: firstResult?.submissionTask?.value,
     };
   }
+}
+
+async function getOrganisationIdFromSubmission(submissionUuid) {
+  const response = await querySudo(`
+    PREFIX mu: <http://mu.semte.ch/vocabularies/core/>
+    PREFIX prov: <http://www.w3.org/ns/prov#>
+    PREFIX pav:  <http://purl.org/pav/>
+
+    SELECT DISTINCT ?organisationId WHERE {
+      ?submission mu:uuid ${sparqlEscapeString(submissionUuid)} .
+      ?job prov:generated ?submission .
+      ?submission pav:createdBy ?bestuurseenheid .
+      ?bestuurseenheid mu:uuid ?organisationId .
+    }
+    LIMIT 1
+  `);
+  return response?.results?.bindings[0]?.organisationId?.value;
 }
